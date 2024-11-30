9aec6c134238a9b5178c2575f740be31617312d4
Vishal Golhar <135511723+mrvishalg2004@users.noreply.github.com>
2024-11-30 21:03:31 +0530

    Add the inslights-v3

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,41 @@
+// import { Inter } from "next/font/google";
+// import "./globals.css";
+// import { ClerkProvider } from "@clerk/nextjs";
+// import { Toaster } from "@/components/ui/sonner";
+// import { SpeedInsights } from "@vercel/speed-insights/next";
+// const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata = {
+//   title: "Interview Pro",
+//   description: "Develope by PV software solution",
+// };
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <ClerkProvider>
+//       <html lang="en">
+//         <body className={inter.className}>
+//           <Toaster />
+//           {children}
+//         </body>
+//       </html>
+//     </ClerkProvider>
+//   );
+// }
+
+
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"; // Import SpeedInsights
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Interview Pro",
-  description: "Develope by PV software solution",
+  description: "Developed by PV software solution",
 };
 
 export default function RootLayout({ children }) {
@@ -17,6 +45,8 @@ export default function RootLayout({ children }) {
         <body className={inter.className}>
           <Toaster />
           {children}
+          {/* Add the SpeedInsights component to the layout */}
+          <SpeedInsights />
         </body>
       </html>
     </ClerkProvider>
